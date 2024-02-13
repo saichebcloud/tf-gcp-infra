607051edbb57b25588e56fe1758718394d696b36
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-02-12 23:13:16 -0500

    initial terraform code

---
+++ infra/variables.tf
@@ -0,0 +1,23 @@
+variable "gcp_svc_key" {
+  type = string
+}
+
+variable "gcp_project" {
+  type = string
+}
+
+variable "gcp_region" {
+  type = string
+}
+
+variable "vpc_network_name" {
+  type = string
+}
+
+variable "webapp_ip_cidr_range" {
+  type = string
+}
+
+variable "db_ip_cidr_range" {
+  type = string
+}
