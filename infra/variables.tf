de7ec5c9492fa16751e65ea022c393bf29924d9b
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-02-14 14:28:31 -0500

    added workflow for terraform validate and var changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,10 +14,50 @@ variable "vpc_network_name" {
   type = string
 }
 
-variable "webapp_ip_cidr_range" {
+variable "subnet_1_ip_cidr_range" {
   type = string
 }
 
-variable "db_ip_cidr_range" {
+variable "subnet_2_ip_cidr_range" {
   type = string
+}
+
+variable "route_name" {
+  type    = string
+  default = "custom-created"
+}
+
+variable "subnet_1_name" {
+  type    = string
+  default = "webapp"
+}
+
+variable "subnet_2_name" {
+  type    = string
+  default = "db"
+}
+
+variable "routing_mode" {
+  type    = string
+  default = "REGIONAL"
+}
+
+variable "auto_create_subnet_bool" {
+  type    = bool
+  default = false
+}
+
+variable "delete_default_routes" {
+  type    = bool
+  default = true
+}
+
+variable "route_dest_range" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "next_hop_gateway" {
+  type    = string
+  default = "default-internet-gateway"
 }
