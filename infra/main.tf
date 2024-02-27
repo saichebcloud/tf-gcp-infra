64c2ce7006e593daed4a867db3de59abc8d38661
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-02-27 12:37:15 -0500

    sql support

--- infra/main.tf
+++ infra/main.tf
@@ -24,6 +24,22 @@ resource "google_compute_subnetwork" "subnet_2" {
 
 }
 
+resource "google_compute_global_address" "private_ip_address" {
+
+  name          = var.internal_ip_config_name
+  address_type  = var.ip_address_type
+  purpose       = var.address_purpose
+  network       = google_compute_network.vpc_network.id
+  prefix_length = var.address_prefix_length
+
+}
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = var.google_network_service
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
 resource "google_compute_route" "route" {
 
   name             = "${var.route_name}-route"
@@ -88,4 +104,71 @@ resource "google_compute_instance" "my_instance" {
 
   zone = var.vm_zone
 
+
+  depends_on = [google_sql_database.my_database]
+
+  metadata = {
+    DB_NAME     = google_sql_database.my_database.name
+    DB_HOST     = google_sql_database_instance.sql_instance.private_ip_address
+    DB_USER     = google_sql_user.my_sql_user.name
+    DB_PASSWORD = random_password.mysql_password.result
+  }
+
+  metadata_startup_script = <<-SCRIPT
+
+  #!/bin/bash
+
+  echo "DB_NAME=${google_sql_database.my_database.name}" > /tmp/.env
+  echo "DB_HOST=${google_sql_database_instance.sql_instance.private_ip_address}" >> /tmp/.env
+  echo "DB_PORT=3306" >> /tmp/.env
+  echo "DB_USER=${google_sql_user.my_sql_user.name}" >> /tmp/.env
+  echo "DB_PASSWORD=${random_password.mysql_password.result}" >> /tmp/.env
+
+  cp /tmp/.env /home/csye6225/.env
+
+  SCRIPT
+
+}
+
+resource "random_integer" "random_generated_int" {
+  min = var.random_int_min
+  max = var.random_int_max
+}
+
+resource "google_sql_database_instance" "sql_instance" {
+
+  name                = "${var.my_sql_instance_name}-${random_integer.random_generated_int.result}"
+  database_version    = var.my_sql_version
+  deletion_protection = var.deletion_protection
+  depends_on          = [google_service_networking_connection.default]
+  settings {
+    tier              = var.my_sql_tier
+    availability_type = var.my_sql_availability
+    disk_type         = var.my_sql_disk_type
+    disk_size         = var.my_sql_disk_size
+    ip_configuration {
+      ipv4_enabled                                  = var.my_sql_public
+      private_network                               = google_compute_network.vpc_network.id
+      enable_private_path_for_google_cloud_services = var.my_sql_path_google_service
+    }
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database" "my_database" {
+  name     = var.my_sql_db_name
+  instance = google_sql_database_instance.sql_instance.name
+}
+
+resource "random_password" "mysql_password" {
+  length = var.random_password_length
+}
+
+resource "google_sql_user" "my_sql_user" {
+  name     = var.my_sql_username
+  instance = google_sql_database_instance.sql_instance.name
+  password = random_password.mysql_password.result
 }
